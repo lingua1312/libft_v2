e159f0a96f6ec31640f24128444fd49a99d01bd0
Nils Sichtig <nsichtig@1-D-28.42heilbronn.de>
2025-03-12 16:54:18 +0100

    added memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,19 +6,25 @@
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:18:09 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/12 14:39:58 by nsichtig         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:07:16 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*temp_dst;
 	unsigned char	*temp_src;
 
-	if (dst == (void*)0 && src == (void *)0)
+	if (dst == (void *)0 && src == (void *)0)
+		return (dst);
+	tmp_dst = (unsigned char *) dst;
+	tmp_src = (unsigned char *) src;
+	while (n > 0)
+	{
+		*(tmp_dst++) = *(tmp_src++);
+		n--;
+	}
 	return (dst);
-
-
 }

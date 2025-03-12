d8b56ef927d00f9734d3227d40eb6c708c83fb53
Nils Sichtig <nsichtig@1-D-28.42heilbronn.de>
2025-03-12 15:59:57 +0100

    added bzero and memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 15:18:09 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/12 14:39:58 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*temp_dst;
+	unsigned char	*temp_src;
+
+	if (dst == (void*)0 && src == (void *)0)
+	return (dst);
+
+
+}

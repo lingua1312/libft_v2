4de91a132c6520dc3e51d5575174004d71599633
Nils Sichtig <nsichtig@1-E-1.42heilbronn.de>
2025-03-17 16:52:04 +0100

    changed atoi to const char

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:27:11 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/10 18:52:59 by nsichtig         ###   ########.fr       */
+/*   Updated: 2025/03/14 13:47:38 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	*ft_memset(void *b, int c, size_t length)
 	unsigned char	src;
 	size_t			i;
 
-	new_dest = dest;
+	new_dest = b;
 	src = c;
 	i = 0;
 	while (i++ < length)
 		*new_dest++ = src;
-	return (dest);
+	return (b);
 }

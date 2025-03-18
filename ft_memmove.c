fe28d3c42d7418d865decd7a97feb0c2307fb70c
Nils Sichtig <nsichtig@1-E-31.42heilbronn.de>
2025-03-19 00:31:54 +0100

    added memmove

---
+++ ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 16:07:52 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/16 21:37:06 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*c_dst;
+	char		*c_src;
+	size_t		i;
+
+	if (!dst && !src)
+		return (NULL);
+	c_dst = (char *)dst;
+	c_src = (char *)src;
+	i = 0;
+	if (c_dst > c_src)
+	{
+		while (len-- > 0)
+			c_dst[len] = c_src[len];
+	}
+	else
+	{
+		while (i < len)
+		{
+			c_dst[i] = c_src[i];
+			i++;
+		}
+	}
+	return (dst);
+}

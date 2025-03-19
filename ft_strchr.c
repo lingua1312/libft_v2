2725beb051c9274627d5882848b9deecd5263e41
Nils Sichtig <nsichtig@1-E-31.42heilbronn.de>
2025-03-19 06:11:45 +0100

    added strncmp

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:10:25 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/19 02:56:29 by nsichtig         ###   ########.fr       */
+/*   Updated: 2025/03/19 04:17:37 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,58 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "libft.h"
+
+// int	main(void)
+// {
+// 	char	*str1;
+// 	char	*str2;
+// 	char	*str3;
+// 	char	*str4;
+// 	char	*result;
+
+// 	str1 = "   Hello, World!   ";
+// 	str2 = " \t\n";
+// 	str3 = "xyz123xyz";
+// 	str4 = "xyz";
+
+// 	printf("Test 1: Trimming spaces and tabs\n");
+// 	result = ft_strtrim(str1, str2);
+// 	printf("Original: \"%s\"\n", str1);
+// 	printf("Set: \"%s\"\n", str2);
+// 	printf("Result: \"%s\"\n\n", result);
+// 	free(result);
+
+// 	printf("Test 2: Trimming specific characters\n");
+// 	result = ft_strtrim(str3, str4);
+// 	printf("Original: \"%s\"\n", str3);
+// 	printf("Set: \"%s\"\n", str4);
+// 	printf("Result: \"%s\"\n\n", result);
+// 	free(result);
+
+// 	printf("Test 3: Nothing to trim\n");
+// 	result = ft_strtrim("Hello", "xyz");
+// 	printf("Original: \"%s\"\n", "Hello");
+// 	printf("Set: \"%s\"\n", "xyz");
+// 	printf("Result: \"%s\"\n\n", result);
+// 	free(result);
+
+// 	printf("Test 4: Empty string\n");
+// 	result = ft_strtrim("", "abc");
+// 	printf("Original: \"%s\"\n", "");
+// 	printf("Set: \"%s\"\n", "abc");
+// 	printf("Result: \"%s\"\n\n", result);
+// 	free(result);
+
+// 	printf("Test 5: Everything trimmed\n");
+// 	result = ft_strtrim("abababa", "ab");
+// 	printf("Original: \"%s\"\n", "abababa");
+// 	printf("Set: \"%s\"\n", "ab");
+// 	printf("Result: \"%s\"\n", result);
+// 	free(result);
+
+// 	return (0);
+// }
---
+++ ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 11:02:32 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/19 06:06:10 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s1[i]);
+}

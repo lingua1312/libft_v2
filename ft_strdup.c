e1795062faa739c814d172c7a156b3ff97f886ce
Nils Sichtig <nsichtig@1-E-31.42heilbronn.de>
2025-03-19 03:45:03 +0100

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,11 @@
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:23:54 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/17 23:13:41 by nsichtig         ###   ########.fr       */
+/*   Updated: 2025/03/19 02:19:39 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strdup(const char *src)
 {
@@ -22,9 +21,7 @@ char	*ft_strdup(const char *src)
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (!result)
 		return (NULL);
-
 	ft_memcpy(result, src, len);
 	result[len] = '\0';
-
 	return (result);
 }

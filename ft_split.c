dc4575b377d1f7fe72fefd69eef004369606a28b
Nils Sichtig <nsichtig@1-E-1.42heilbronn.de>
2025-03-20 04:30:37 +0100

    added split

---
+++ ft_split.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 15:02:20 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/20 02:46:03 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(const char *s, char c)
+{
+	size_t	count;
+	int		in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s == c)
+			in_word = 0;
+		else if (!in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		s++;
+	}
+	return (count);
+}
+
+static size_t	word_len(const char *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
+
+static char	*make_word(const char *s, size_t len)
+{
+	char	*word;
+	size_t	i;
+
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static char	**process_split(char const *s, char c, char **result, size_t count)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (i < count)
+	{
+		while (*s == c)
+			s++;
+		len = word_len(s, c);
+		result[i] = make_word(s, len);
+		if (!result[i])
+		{
+			while (i > 0)
+				free(result[--i]);
+			free(result);
+			return (NULL);
+		}
+		s += len;
+		i++;
+	}
+	result[i] = NULL;
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	size_t	word_count;
+
+	if (!s)
+		return (NULL);
+	word_count = count_words(s, c);
+	result = (char **)malloc(sizeof(char *) * (word_count + 1));
+	if (!result)
+		return (NULL);
+	return (process_split(s, c, result, word_count));
+}

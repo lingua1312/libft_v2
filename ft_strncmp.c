b1b658b87dc5df39f13ca3ff9c1407fb2e1fc468
Nils Sichtig <nsichtig@1-E-1.42heilbronn.de>
2025-03-20 02:13:01 +0100

    was falsely substracting s1 from itself, changed to s2

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 11:02:32 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/19 06:06:10 by nsichtig         ###   ########.fr       */
+/*   Updated: 2025/03/20 02:11:16 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] == s2[i] && s1[i] && i < n - 1)
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s1[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }

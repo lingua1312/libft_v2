5c85b32c6204f4a3c9bd77a07e9cbf7f8a29118d
Nils Sichtig <nsichtig@1-E-29.42heilbronn.de>
2025-03-21 19:33:11 +0100

    added strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 15:02:57 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/21 19:32:46 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*result;
+	unsigned char	i;
+
+	if (!s || !f)
+		return (NULL);
+
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!result)
+		return (NULL);
+
+	i = 0;
+	while (s[i])
+	{
+		result [i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+
+	return (result);
+}
+// #include "libft.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // Sample function to capitalize every alternate character
+// char sample_function(unsigned int index, char c)
+// {
+// 	if (index % 2 == 0)
+// 		return (c - 32); // Convert lowercase to uppercase (if applicable)
+// 	return (c);
+// }
+
+// int main(void)
+// {
+// 	char *original = "abcdef";
+// 	char *modified;
+
+// 	modified = ft_strmapi(original, sample_function);
+// 	if (!modified)
+// 	{
+// 		printf("Memory allocation failed.\n");
+// 		return (1);
+// 	}
+
+// 	printf("Original: %s\n", original);
+// 	printf("Modified: %s\n", modified);
+
+// 	free(modified);
+// 	return (0);
+// }

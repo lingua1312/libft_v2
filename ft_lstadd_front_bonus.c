a599806e60a2cd2920f801e7eed08d30cdbd8deb
Nils Sichtig <nsichtig@1-E-1.42heilbronn.de>
2025-03-24 16:36:50 +0100

    added lstadd_front

---
+++ ft_lstadd_front_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 16:01:29 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/24 16:36:10 by nsichtig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	temp = *lst;
+	*lst = new;
+	new->next = temp;
+}

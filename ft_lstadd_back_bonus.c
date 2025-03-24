c915b999654bf16007583690f00ad686890b0bd2
Nils Sichtig <nsichtig@1-A-32.42heilbronn.de>
2025-03-24 21:45:26 +0100

    update

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,22 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/24 16:01:29 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/24 21:44:16 by nsichtig         ###   ########.fr       */
+/*   Created: 2025/03/24 19:21:28 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/24 21:45:07 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*temp;
+	t_list	*last;
 
-	temp = *lst;
-	*lst = new;
-	new->next = temp;
+
 }
--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsichtig <nsichtig@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/24 16:01:29 by nsichtig          #+#    #+#             */
-/*   Updated: 2025/03/24 21:44:16 by nsichtig         ###   ########.fr       */
+/*   Created: 2025/03/24 21:33:54 by nsichtig          #+#    #+#             */
+/*   Updated: 2025/03/24 21:33:55 by nsichtig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	t_list	*temp;
-
-	temp = *lst;
-	*lst = new;
-	new->next = temp;
-}
